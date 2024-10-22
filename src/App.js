c178739d4d1ddcdd123cd3f4abf7bb2bcb7adb82
altan-ab <altan.abduloglu@gmail.com>
2024-10-22 20:26:50 +0300

    initial-setup

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 // Bileşenin bağlantısı kaldırıldığında/unmounted uygulamamızın state'i güncellemesini durdurmak için useEffect'teki cleanup fonksiyonunu kullanın
+
+const clientWindow = typeof window !== 'undefined'
+
 export default function App() {
-  const [isWindowPosition, setIsWindowPosition] = useState(false)
-
-  function toggleWindowSize() {
-    setIsWindowPosition((pre) => !pre)
-  }
-
   return (
     <div className="flex justify-center flex-col items-center py-8">
       <h1 className="text-2xl font-bold pb-4">🐭</h1>
-      <BrowserRouter>
-        <nav className="flex justify-center max-w-sm p-4 pb-8 space-x-4">
-          <Link className="underline" to="/">
-            Home
-          </Link>
-          <Link className="underline" to="/about">
-            About
-          </Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={toggleWindowSize && <MousePosition />} />
-          <Route
-            path="/about"
-            element={<h1>Mouse&apos;unuzu takip edin!</h1>}
-          />
-        </Routes>
-      </BrowserRouter>
+      {clientWindow ? (
+        <BrowserRouter>
+          <nav className="flex justify-center max-w-sm p-4 pb-8 space-x-4">
+            <Link className="underline" to="/">
+              Home
+            </Link>
+            <Link className="underline" to="/about">
+              About
+            </Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MousePosition />} />
+            <Route
+              path="/about"
+              element={<h1>Mouse&apos;unuzu takip edin!</h1>}
+            />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <h1>Tarayıcı fonksiyonları kullanılamaz.</h1>
+      )}
     </div>
   )
 }
@@ -37,19 +38,15 @@ function MousePosition() {
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    function handleMove(e) {
-      if (typeof window !== 'undefined') {
+    if (clientWindow) {
+      function handleMove(e) {
         setPosition({ x: e.clientX, y: e.clientY })
         console.log('Updating state')
       }
-    }
-    //vercel hatası sonucu farkındalık: window nesnesi yalnızca tarayıcıda çalıştığı için, sunucu tarafında çalıştırıldıklarında hata verir. Bunu kontrol etmek için typeof window ekledim.
-    if (typeof window !== 'undefined') {
       window.addEventListener('pointermove', handleMove) //mouse hareketleri takip event listener ile
-    }
-    //cleanup fonksiyonu ⬇️ : bileşen unmount olunca çalışır
-    return () => {
-      if (typeof window !== 'undefined') {
+
+      //cleanup fonksiyonu ⬇️ : bileşen unmount olunca çalışır
+      return () => {
         console.log('Unmounted-state güncellenmiyor')
         window.removeEventListener('pointermove', handleMove)
       }
@@ -57,7 +54,8 @@ function MousePosition() {
   }, [])
 
   /*
-
+  //vercel hatası sonucu farkındalık: window nesnesi yalnızca tarayıcıda çalıştığı için, sunucu tarafında çalıştırıldıklarında hata verir. Bunu kontrol etmek için typeof window ekledim.
+  
   useEffect'te cleanup fonksiyonu, bileşen unmount olduğunda, yani DOM’dan kaldırıldığında çalışan bir geri çağırma fonksiyonudur. Bu fonksiyon, event listener’ı kaldırmak ve gereksiz state güncellemelerinin önüne geçmek için kullanılır.
 
   */
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+'use strict'
 
 import App from '@/App'
 
